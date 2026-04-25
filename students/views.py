bd9fcd7f877b9db4073c5f2de13da8ba6bdcd2cf
RakibmAmmar <rakibmdammar@gmail.com>
2026-04-25 13:28:53 +0600

    Added student registration form and navigation

--- students/views.py
+++ students/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Student
+from django.shortcuts import render, redirect # Add redirect here
+from .forms import StudentForm # Add this import
 
-# Create your views here.
+def student_list(request):
+    # This line grabs ALL students from the database
+    all_students = Student.objects.all()
+    # This line sends those students to an HTML file (template)
+    return render(request, 'students/student_list.html', {'students': all_students})
+
+def add_student(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list') # Go back to the list after saving
+    else:
+        form = StudentForm()
+    return render(request, 'students/add_student.html', {'form': form})
